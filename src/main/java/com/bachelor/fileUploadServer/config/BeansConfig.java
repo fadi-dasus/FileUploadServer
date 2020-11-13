999746763eb138b37789f1e2997798737a3f910a
fadi dasus <31960592+fadi-dasus@users.noreply.github.com>
2020-11-13 17:54:11 +0100

    refactoring the service layer

---
+++ src/main/java/com/bachelor/fileUploadServer/config/BeansConfig.java
@@ -0,0 +1,13 @@
+package com.bachelor.fileUploadServer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class BeansConfig {
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+}
--- src/main/java/com/bachelor/fileUploadServer/service/FileService.java
+++ src/main/java/com/bachelor/fileUploadServer/service/FileService.java
@@ -1,33 +1,50 @@
 package com.bachelor.fileUploadServer.service;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import org.springframework.beans.factory.annotation.Value;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.bachelor.fileUploadServer.config.CONSTANTS;
 import com.bachelor.fileUploadServer.exception.FileStorageException;
 
 @Service
 public class FileService {
+	@Autowired
+	RestTemplate restTemplate;
 
-    @Value("${app.upload.dir:${user.home}}")
-    public String uploadDir;
 
-    public void uploadFile(MultipartFile file) {
+	public void uploadFile(MultipartFile file) {
+		try {
+			moveTheFileToTheDirectory(file, getTheTempFolderDirectory());
+		} catch (Exception e) {
+			catchException(e, file);
+		}
+	}
 
-        try {
-            Path copyLocation = Paths
-                .get(uploadDir + File.separator + StringUtils.cleanPath(file.getOriginalFilename()));
-            Files.copy(file.getInputStream(), copyLocation, StandardCopyOption.REPLACE_EXISTING);
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new FileStorageException("Could not store file " + file.getOriginalFilename()
-                + ". Please try again!");
-        }
-    }
+	private void moveTheFileToTheDirectory(MultipartFile file, String uploadDir) throws IOException {
+		Path copyLocation = Paths.get(uploadDir + File.separator + StringUtils.cleanPath(file.getOriginalFilename()));
+		Files.copy(file.getInputStream(), copyLocation, StandardCopyOption.REPLACE_EXISTING);
+	}
+
+	private String getTheTempFolderDirectory() {
+		ResponseEntity<String> response = restTemplate.getForEntity(CONSTANTS.URL, String.class);
+		return response.getBody();
+	}
+
+	private void catchException(Exception e, MultipartFile file) {
+		e.printStackTrace();
+		throw new FileStorageException(
+				"Could not store the file " + file.getOriginalFilename() + ". Please try again!");
+	}
+
 }
